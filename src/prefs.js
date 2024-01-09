e8683d5c7c839c322a199aa596e0274d4474d211
Philippe Troin <phil+github-commits@fifi.org>
2024-01-09 12:17:25 -0800

    Update Copyright notice.

--- src/prefs.js
+++ src/prefs.js
@@ -1,5 +1,5 @@
 // weeks-start-on-monday - Gnome shell extension for changing the week start day
-// Copyright (C) 2019-2022, 2024 Philippe Troin (F-i-f on Github)
+// Copyright (C) 2019-2024 Philippe Troin (F-i-f on Github)
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -108,7 +108,7 @@ const WeeksStartOnMondaySettings = GObject.registerClass(class WeeksStartOnMonda
 	this.copyright_label = new Gtk.Label({
 	    use_markup: true,
 	    label: '<span size="small">'
-		+ _('Copyright © 2019-2022 Philippe Troin (<a href="https://github.com/F-i-f">F-i-f</a> on GitHub)')
+		+ _('Copyright © 2019-2024 Philippe Troin (<a href="https://github.com/F-i-f">F-i-f</a> on GitHub)')
 		+ '</span>',
 	    hexpand: true,
 	    halign: Gtk.Align.CENTER,
