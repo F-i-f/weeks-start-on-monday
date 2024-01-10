9ef2c7e2fbebad5691c8d75fba2eda5d31f299bf
Philippe Troin <phil+github-commits@fifi.org>
2024-01-10 06:38:45 -0800

    Convert ExtensionPreferences member variables to local variables.
    Release 15 was rejected by GS maintainers because of object lifetime
    issues.

--- src/prefs.js
+++ src/prefs.js
@@ -22,100 +22,97 @@ import Gtk                                  from 'gi://Gtk';
 export default class WeeksStartOnMondaySettings extends ExtensionPreferences {
 
     fillPreferencesWindow(window) {
-	this._grid = new Gtk.Grid();
-	this._grid.margin_top = 12;
-	this._grid.margin_bottom = this._grid.margin_top;
-	this._grid.margin_start = 48;
-	this._grid.margin_end = this._grid.margin_start;
-	this._grid.row_spacing = 6;
-	this._grid.column_spacing = this._grid.row_spacing;
-	this._grid.orientation = Gtk.Orientation.VERTICAL;
+	const grid = new Gtk.Grid();
+	grid.margin_top = 12;
+	grid.margin_bottom = grid.margin_top;
+	grid.margin_start = 48;
+	grid.margin_end = grid.margin_start;
+	grid.row_spacing = 6;
+	grid.column_spacing = grid.row_spacing;
+	grid.orientation = Gtk.Orientation.VERTICAL;
 
-	this._settings = this.getSettings();
+	const settings = this.getSettings();
 
 	let ypos = 1;
-	let descr;
 
-	this.title_label = new Gtk.Label({
+	const title_label = new Gtk.Label({
 	    use_markup: true,
 	    label: '<span size="large" weight="heavy">'
 		+_('Weeks Start on Monday Again...')+'</span>',
 	    hexpand: true,
 	    halign: Gtk.Align.CENTER
 	});
-	this._grid.attach(this.title_label, 1, ypos, 2, 1);
+	grid.attach(title_label, 1, ypos, 2, 1);
 
 	ypos += 1;
 
-	this.version_label = new Gtk.Label({
+	const version_label = new Gtk.Label({
 	    use_markup: true,
 	    label: '<span size="small">'+_('Version')
 		+ ' ' + this.metadata['version']+' / git '+this.metadata['vcs_revision'] + '</span>',
 	    hexpand: true,
 	    halign: Gtk.Align.CENTER,
 	});
-	this._grid.attach(this.version_label, 1, ypos, 2, 1);
+	grid.attach(version_label, 1, ypos, 2, 1);
 
 	ypos += 1;
 
-	this.link_label = new Gtk.Label({
+	const link_label = new Gtk.Label({
 	    use_markup: true,
 	    label: '<span size="small"><a href="'+this.metadata.url+'">'
 		+ this.metadata.url + '</a></span>',
 	    hexpand: true,
 	    halign: Gtk.Align.CENTER,
-	    margin_bottom: this._grid.margin_bottom
+	    margin_bottom: grid.margin_bottom
 	});
-	this._grid.attach(this.link_label, 1, ypos, 2, 1);
+	grid.attach(link_label, 1, ypos, 2, 1);
 
 	ypos += 1;
 
-	descr = _(this._settings.settings_schema.get_key('start-day').get_description());
-	this.starton_label = new Gtk.Label({label: _("Weeks start on:")});
-	this.starton_label.set_tooltip_text(descr);
-	this.starton_adjustment = new Gtk.Adjustment({ lower          : 0,
-						       upper          : 6,
-						       step_increment : 1
-					      })
-	this.starton_control = new Gtk.Scale({orientation: Gtk.Orientation.VERTICAL,
-					      draw_value: false,
-					      margin_start: 5,
-					      margin_end: 5,
-					      margin_top: 5,
-					      margin_bottom: 5,
-					      digits: 0,
-					      adjustment: this.starton_adjustment
-					     });
-	this.starton_control.set_tooltip_text(descr);
-	this.starton_control.set_round_digits(0);
+	const starton_descr = _(settings.settings_schema.get_key('start-day').get_description());
+	const starton_label = new Gtk.Label({label: _("Weeks start on:")});
+	starton_label.set_tooltip_text(starton_descr);
+	const starton_adjustment = new Gtk.Adjustment({ lower          : 0,
+							upper          : 6,
+							step_increment : 1
+						      })
+	const starton_control = new Gtk.Scale({orientation: Gtk.Orientation.VERTICAL,
+					       draw_value: false,
+					       margin_start: 5,
+					       margin_end: 5,
+					       margin_top: 5,
+					       margin_bottom: 5,
+					       digits: 0,
+					       adjustment: starton_adjustment
+					      });
+	starton_control.set_tooltip_text(starton_descr);
+	starton_control.set_round_digits(0);
 	[ _('Sunday'), _('Monday'), _('Tuesday'), _('Wednesday'),
 	  _('Thursday'), _('Friday'), _('Saturday')
 	].forEach((function(day, idx) {
-	    this.starton_control.add_mark(idx, Gtk.PositionType.BOTTOM, day);
+	    starton_control.add_mark(idx, Gtk.PositionType.BOTTOM, day);
 	}).bind(this));
-	let css = new Gtk.CssProvider();
-	this.starton_control.get_style_context().add_provider(css, Gtk.StyleProvider.PRIORITY_APPLICATION);
-	this._grid.attach(this.starton_label,   1, ypos, 1, 1);
-	this._grid.attach(this.starton_control, 2, ypos, 1, 1);
-	this._settings.bind('start-day', this.starton_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+	grid.attach(starton_label,   1, ypos, 1, 1);
+	grid.attach(starton_control, 2, ypos, 1, 1);
+	settings.bind('start-day', starton_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
 
 	ypos += 1;
 
-	this.copyright_label = new Gtk.Label({
+	const copyright_label = new Gtk.Label({
 	    use_markup: true,
 	    label: '<span size="small">'
 		+ _('Copyright © 2019-2024 Philippe Troin (<a href="https://github.com/F-i-f">F-i-f</a> on GitHub)')
 		+ '</span>',
 	    hexpand: true,
 	    halign: Gtk.Align.CENTER,
-	    margin_top: this._grid.margin_top
+	    margin_top: grid.margin_top
 	});
-	this._grid.attach(this.copyright_label, 1, ypos, 2, 1);
+	grid.attach(copyright_label, 1, ypos, 2, 1);
 
 	ypos += 1;
 
 	const group = new Adw.PreferencesGroup();
-	group.add(this._grid);
+	group.add(grid);
 	const page = new Adw.PreferencesPage();
 	page.add(group);
 
